e27b64ff2184dbd390e16475137cf48e114ae07c
JoseNguyen <abc@example.com>
2022-01-26 19:40:35 +0700

    home

--- app/src/main/java/com/example/appsales18102021/data/datasource/remote/api/ApiRequest.java
+++ app/src/main/java/com/example/appsales18102021/data/datasource/remote/api/ApiRequest.java
@@ -1,10 +1,14 @@
 package com.example.appsales18102021.data.datasource.remote.api;
 
 import com.example.appsales18102021.data.datasource.remote.AppResource;
+import com.example.appsales18102021.data.model.FoodModel;
 import com.example.appsales18102021.data.model.UserModel;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiRequest {
@@ -15,6 +19,6 @@ public interface ApiRequest {
     @POST("api/v1/user/sign-up")
     Call<AppResource<UserModel>> signUp(@Body UserModel userModel);
 
-    @POST("api/v1/user/splash")
-    Call<AppResource<UserModel>> splash(UserModel userModel);
+    @GET("api/v1/food/list/0/10")
+    Call<AppResource<List<FoodModel>>> fetchListFoods();
 }
